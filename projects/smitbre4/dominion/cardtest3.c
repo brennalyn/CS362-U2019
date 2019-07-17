9d08cbdacecec37fdaccd16a469c108b39037b0e
b9iv <brenna.smith@nordstrom.com>
2019-07-16 21:05:15 -0700

    Git issues

--- projects/smitbre4/dominion/cardtest3.c
+++ projects/smitbre4/dominion/cardtest3.c
@@ -1,9 +1,6 @@
 /* -----------------------------------------------------------------------
- * Demonstration of how to write unit tests for dominion-base
- * Include the following lines in your makefile:
- *
- * testUpdateCoins: testUpdateCoins.c dominion.o rngs.o
- *      gcc -o testUpdateCoins -g  testUpdateCoins.c dominion.o rngs.o $(CFLAGS)
+ * Brenna Smith
+ * CS362 2019
  * -----------------------------------------------------------------------
  */
 
